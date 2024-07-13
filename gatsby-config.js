104b752e8a852af81f5da2f392deebfc9ddf7748
harshverma1516 <harshverma1516@gmail.com>
2024-07-13 13:15:12 +0530

    updated the address

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       mobile: `(+91) 9461040282`,
       telephone: ``,
     },
-    address: `B-62 Ground Floor, Lakshyadeep Tower, 80 Feet Rd, Narayan Vihar, Jaipur, Rajasthan 302020, India`,
+    address: `357, Mansarovar Plaza, Madhyam Marg, Mansarovar, Jaipur, Rajasthan 302020, India`,
     map: `https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28474.192359069755!2d75.723648!3d26.863033!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2275753fc988d813!2sChaardivari!5e0!3m2!1sen!2sus!4v1624192717047!5m2!1sen!2sus`,
     opening: {
       day: `All Days`,
